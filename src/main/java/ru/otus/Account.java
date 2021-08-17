117971047bdcadac09620038927c0654493dc5eb
Konstantin Boryagin <nokss89@gmail.com>
2021-08-17 13:39:09 +0300

    new way

--- src/main/java/ru/otus/Account.java
+++ src/main/java/ru/otus/Account.java
@@ -1,41 +1,7 @@
 package ru.otus;
 
-import java.util.HashMap;
 
 public class Account {
 
-    private Client client;
-    private int money;
 
-
-    Manager manager = new Manager();
-
-    public Account(Client client, int money) {
-        this.client = client;
-        this.money = money;
-//        manager.setClientAccount(this);
-//        manager.addAccount(this, client);
-    }
-
-    public Client getClient() {
-        return client;
-    }
-
-
-
-    public void print(HashMap<Integer, Account> accounts){
-        for (Integer element: accounts.keySet()){
-            String key = element.toString();
-            String value = accounts.get(element).toString();
-            System.out.println(key + " " + value);
-        }
-
-    }
-
-    @Override
-    public String toString() {
-        return "Счет " +
-                "клиент - " + client +
-                ", средства - " + money + " | \n";
-    }
 }
--- src/main/java/ru/otus/Client.java
+++ src/main/java/ru/otus/Client.java
@@ -1,52 +1,8 @@
 package ru.otus;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+
 
 public class Client {
 
-    private String name;
-    private LocalDate birthDate;
-    private static List<Account> clientAccounts = new ArrayList<>();
 
-
-    Manager manager = new Manager();
-
-
-
-    public Client(String name, LocalDate birthDate) {
-        this.name = name;
-        this.birthDate = birthDate;
-        clientAccounts = new ArrayList<>();
-        //manager.addClientToClients(this, new ArrayList<>());
-    }
-
-    public void setClientAccount(Account account) {
-        clientAccounts.add(account);
-    }
-
-    public static List<Account> getClientAccounts() {
-        return clientAccounts;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Client client = (Client) o;
-        return name.equals(client.name) && birthDate.equals(client.birthDate);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, birthDate);
-    }
-
-    @Override
-    public String toString() {
-        return  "имя - '" + name + '\'' +
-                ", дата рождения - " + birthDate + " \\ ";
-    }
 }
